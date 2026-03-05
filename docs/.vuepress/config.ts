ef9f32e2b4fb1cabf14c3030f0e995010030a10b
jackie.liu <moklgy@foxmail.com>
2026-03-05 18:01:40 +0800

    用户名是moklgydocs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -350,8 +350,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           clientID: 'Ov23lic9CLCxo81wIXVY',//'Iv23ctuoNjpI14PQi50l',
           clientSecret: 'dc2bf02626d1bbf9a7db2c3380ee39a8fe490aa5',//'90c19b1ece253216c289f0e97230d460320bcf17',
           repo: 'moklgydocs.github.io', // GitHub 仓库
-          owner: 'moklgy', // GitHub仓库所有者
-          admin: ['moklgy'], // 对仓库有写权限的人
+          owner: 'moklgydocs', // GitHub仓库所有者
+          admin: ['moklgydocs'], // 对仓库有写权限的人
           // distractionFreeMode: true,
           pagerDirection: 'last', // 'first'正序 | 'last'倒序
           id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
